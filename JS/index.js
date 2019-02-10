4016ed5f3dc21158d2e2532a8a642d0227f2f507
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-02-10 10:49:36 -0500

    Commented heavily. Deleted unnecessary files, folders, and code. left-up-left... slide effect now accepts odd numbers of images.

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
+// Selects all images from HTML:
 const slideImgs = Array.from(document.querySelectorAll("img"));
-// Sets all slide images to invisible:
+// Sets all images to invisible:
 slideImgs.forEach(image => {
   TweenMax.set(image, { autoAlpha: 0 });
 });
@@ -9,14 +10,15 @@ let slideIn = slideImgs[0];
 TweenMax.set(slideIn, { autoAlpha: 1 });
 // Keeps track of the index of the active slide:
 let slideImgsIndex = 0;
+// Keeps track of whether the next slide animation will move left (0) or up (1):
+let slideDir = 0;
 
 const goToNextSlide = () => {
-  var tl = new TimelineLite();
+  // This GSAP timeline allows for multple animations to happen in sync with one another. In this case the new image slides in and the old image slides out in one fluid motion.
+  const tl = new TimelineLite();
+  // The visible image is marked as the outgoing image:
   let slideOut = slideIn;
-  // Determines slide direction:
-  let slideDir = slideImgsIndex % 2;
-
-  // Determines next slide:
+  // Determines the next incoming image:
   if (slideImgsIndex === slideImgs.length - 1) {
     slideIn = slideImgs[0];
     slideImgsIndex = 0;
@@ -25,32 +27,27 @@ const goToNextSlide = () => {
     slideImgsIndex += 1;
   }
 
-  // Reveals next slide:
-
-  // Keeps track of whether the index is even or odd in order to change the animation direction:
-
+  // Leftward slide animation:
   if (slideDir === 0) {
-    console.log("even");
-    console.log(slideIn);
     tl
-      // move the new slide (the one about to enter viewport) out of the viewport and add class active
+      // Instantly moves the incoming image rightward out of frame and makes it visible (so that it will be visible as it slides into frame):
       .set(slideIn, { x: "100%", autoAlpha: 1 })
-      // animate active slide up (out of the viewport)
-      .to(slideOut, 0.7, { x: "-100%", ease: Power3.easeInOut }, 0)
-      // animate new slide up (from out of the viewport)
-      .to(slideIn, 0.7, { x: "0%", ease: Power3.easeInOut }, 0)
-      // resets slideOut
+      // Animates outgoing image leftward out of frame:
+      .to(slideOut, 0.75, { x: "-100%", ease: Power2.easeInOut }, 0)
+      // Animates incoming image leftward into frame:
+      .to(slideIn, 0.75, { x: "0%", ease: Power2.easeInOut }, 0)
+      // Resets the position and (in)visibility of the image that just exited the frame:
       .set(slideOut, { x: "0%", autoAlpha: 0 });
+    // Sets variable for alternating slide animation direction:
+    slideDir = 1;
+
+    // Upward slide animation:
   } else {
-    console.log("odd");
-    console.log(slideIn);
     tl.set(slideIn, { y: "100%", autoAlpha: 1 })
-      // animate active slide up (out of the viewport)
-      .to(slideOut, 0.7, { y: "-100%", ease: Power3.easeInOut }, 0)
-      // animate new slide up (from out of the viewport)
-      .to(slideIn, 0.7, { y: "0%", ease: Power3.easeInOut }, 0)
-      // resets slideOut
+      .to(slideOut, 0.75, { y: "-100%", ease: Power2.easeInOut }, 0)
+      .to(slideIn, 0.75, { y: "0%", ease: Power2.easeInOut }, 0)
       .set(slideOut, { y: "0%", autoAlpha: 0 });
+    slideDir = 0;
   }
 };
 
